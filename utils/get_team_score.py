b7d4e7d26b0c150eaba6bb770271a0b7a4de45f2
Tyler Lum <tylergwlum@gmail.com>
2019-07-25 10:36:20 -0700

    Try making team and task scores not sums but text

--- utils/get_team_score.py
+++ utils/get_team_score.py
@@ -14,7 +14,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 team_directory = dir_path + '/../logs/' + team_name
 
 # Sum up task scores into team score
-team_score = 0
+team_score = []
 subdirectories = [dI for dI in os.listdir(team_directory) if os.path.isdir(os.path.join(team_directory, dI))]
 for subdirectory in subdirectories:
     # Get task score
@@ -22,13 +22,14 @@ for subdirectory in subdirectories:
     task_score_file = open(task_score_filename, 'r')
     task_score = task_score_file.read()
 
-    # Add it to task score
-    team_score += float(task_score)
+    # Add it to team score
+    team_score.append(task_score)
 
 # Write team score to file
 team_score_filename = team_directory + '/team_score.txt'
 f = open(team_score_filename, 'w+')
-f.write("{}".format(team_score))
+for task_score in team_score:
+    f.write(task_score)
 
 print("Successfully recorded team score in {}".format(team_score_filename))
 f.close()

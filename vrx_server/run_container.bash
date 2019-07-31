a463b68c4d633647823f27d804f56f72e3544da5
Tyler Lum <tylergwlum@gmail.com>
2019-07-31 11:20:01 -0700

    Remove nvidia checks from ARIAC

--- vrx_server/run_container.bash
+++ vrx_server/run_container.bash
@@ -23,7 +23,6 @@ set -x
 
 # Parse arguments
 RUNTIME="runc"
-has_nvidia=0
 
 POSITIONAL=()
 while [[ $# -gt 0 ]]
@@ -33,7 +32,6 @@ do
   case $key in
       -n|--nvidia)
       RUNTIME="nvidia"
-      has_nvidia=1
       shift
       ;;
       *)    # unknown option
@@ -71,37 +69,6 @@ then
     chmod a+r $XAUTH
 fi
 
-# Use lspci to check for the presence of an nvidia graphics card
-# TODO: re-enable nvidia support optionally.
-#has_nvidia=`lspci | grep -i nvidia | wc -l`
-
-# Set docker gpu parameters
-if [ ${has_nvidia} -gt 0 ]
-then
-  # check if nvidia-modprobe is installed
-  if ! which nvidia-modprobe > /dev/null
-  then
-    echo nvidia-docker-plugin requires nvidia-modprobe
-    echo please install nvidia-modprobe
-    exit -1
-  fi
-  # check if nvidia-docker-plugin is installed
-  if curl -s http://localhost:3476/docker/cli > /dev/null
-  then
-    DOCKER_GPU_PARAMS=" $(curl -s http://localhost:3476/docker/cli)"
-    DISPLAY_PARAMS=" -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY"
-  else
-    echo nvidia-docker-plugin not responding on http://localhost:3476/docker/cli
-    echo please install nvidia-docker-plugin
-    echo https://github.com/NVIDIA/nvidia-docker/wiki/Installation
-    exit -1
-  fi
-else
-  DOCKER_GPU_PARAMS=""
-  DISPLAY_PARAMS=""
-fi
-
-DISPLAY="${DISPLAY:-:0}"
 USERID=$(id -u)
 GROUPID=$(id -g)
 
@@ -121,8 +88,6 @@ docker run --name ${CONTAINER} \
   --security-opt seccomp=unconfined \
   -u $USERID:$GROUPID \
   ${DOCKER_EXTRA_ARGS} \
-  ${DOCKER_GPU_PARAMS} \
-  ${DOCKER_DISPLAY_PARAMS} \
   ${IMAGE_NAME} \
 ${COMMAND}
 

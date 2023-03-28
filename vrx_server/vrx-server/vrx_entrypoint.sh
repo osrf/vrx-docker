8097571004454f6a565fad4737ba1af6417abcc1
M1chaelM <mrmccarr@nps.edu>
2023-03-28 15:30:47 -0400

    update paths in vrx_entrypoint.sh

--- vrx_server/vrx-server/vrx_entrypoint.sh
+++ vrx_server/vrx-server/vrx_entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
 
-# first, execute overriden entrypoint from gazebo
-source "/usr/share/gazebo/setup.sh"
-
 # setup ros environment.
-source "/opt/ros/noetic/setup.bash" > /dev/null
+source "/opt/ros/humble/setup.bash" > /dev/null
 
 # setup vrx environment
 source ~/vrx_ws/devel/setup.sh
